359ce8b79ae39369dee2c5d178cded21888c4282
ersi0571 <erwan.simon@inserm.fr>
2021-09-06 14:27:09 +0200

    ajout de la declaration de la fonction twoby2

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -5,6 +5,15 @@
 import numpy as np
 from scipy.stats.distributions import norm
 
+# calcul du tableau des relations-croisées (odds-ratio) à l'instar de la fonction twoby2() du langage 
+
+def twoby2():
+    #parametrer la fonction avec a,b,c et d des cas-temoins (application : glaucome et cataracte du diabetique, DOI?)
+    # renvoyer le ratio et le petit p 
+    
+    return(0)
+    
+
 
 print("hello world !")
 
