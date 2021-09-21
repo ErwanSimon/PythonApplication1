944800ace95335fb7369c74411f2595ed1ca9677
ircan <ircan@lacass73.unice.fr>
2021-09-21 15:09:11 +0200

    inclusion de la fonction de calcul du score de relation croisees

--- pymath.py
+++ pymath.py
@@ -1,9 +1,16 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+# coding: utf-8
 
-# -*- coding: utf-8 -*-
+import math
 
 import numpy as np
-from scipy.stats.distributions import norm
+
+import scipy
+import scipy.stats as stats
+
+import matplotlib.pyplot as plt
+
+#from scipy.stats.distributions import norm
 
 # calcul du tableau des relations-croisées (odds-ratio) à l'instar de la fonction twoby2() du langage R via la librairie Epi
 
@@ -17,17 +24,11 @@ def twoby2():
     # calcul de l'intervalle de confiance à 95% 5SE : Standart Error): SE = sqrt(1/a + 1/b + 1/c + 1/d) ln(OR) <= ln(OR2) + 1.96 * SE
     # print ( exp(ln(OR))
 
+    # stats.fish-exact(input)
+    # return (oddsratio, pvalue)
 
     return(0)
     
-
-
-import matplotlib.pyplot as plt
-import numpy as np
-
-x = np.linspace(-5,5,100)
-plt.plot(x,np.sin(x))
-plt.show()
 
 #def carre(x)
 # return x*x
@@ -35,6 +36,33 @@ plt.show()
 #x=2
 #print  carre(x);
 
-plt.plot()
-plt.show()
+# (lecture du fichier CSV comme dataframe avec pandas)
 
+
+# INTERVALLES DE CONFIANCE
+# calcul simple d'un intervalle de confiance à 95%
+
+moyenne = 38.9
+ecart_type = 13.28
+effectif = 797 # echantillon effectivement mesuree , c.à.d sans les NA !
+
+intervalle_inf = moyenne - 1.96 * ecart_type / math.sqrt(effectif)
+intervalle_sup = moyenne + 1.96 * ecart_type / math.sqrt(effectif)
+
+print("intervalle inferieur : ", intervalle_inf, "intervalle superieur : ", intervalle_sup)
+
+# Score de relations croisées - étude cas-témoins
+# Odds-Ratio
+
+a = np.array([[840, 51663],[32, 5053]])
+oddsratio, pvalue = stats.fisher_exact(a)
+print("OddsR: ", oddsratio, "p-Value:", pvalue)
+#>>> OddsR:  2.56743220487 p-Value: 2.72418938361e-09
+
+# ecriture du ficher dataframe avec des entiers
+np.savetxt("calcul_oddsratio.csv", a, delimiter=';',fmt='%d')
+
+# affichage des donnees (histogramme ou boite a moustache pour l'etude dont l'odds-ratio (score de relations croisées) a été calculé
+# x = np.linspace(-5,5,100)
+# plt.plot(x,np.sin(x))
+# plt.show()
